0d4d59333d9a23494a4ad4604b6f0f3434a83934
Hódi Lajos Gábor <115459590+szt2hodlaj@users.noreply.github.com>
2023-09-19 12:03:16 +0200

    Add files via upload

---
+++ IWont.java
@@ -0,0 +1,7 @@
+public class IWont {
+    public static void main(String[] args){
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Nem fogok csalni a vizsgán!");
+        }
+    }
+}
