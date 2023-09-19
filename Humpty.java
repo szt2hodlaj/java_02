2c0020a00426a301c414eb3e8eb715cf7ba5c424
Hódi Lajos Gábor <115459590+szt2hodlaj@users.noreply.github.com>
2023-09-19 12:02:36 +0200

    Add files via upload

---
+++ Humpty.java
@@ -0,0 +1,8 @@
+public class Humpty {
+    public static void main(String[] args){
+        System.out.println("Minden királyi ló és minden királyi ember");
+        System.out.println("Nézte, hogy Humpty Dumpty nagyot esett.");
+        System.out.println("Humpty Dumpty ült a falon,");
+        System.out.println("Nem tudták újra összerakni Humpty Dumpty-t.");
+    }
+}
