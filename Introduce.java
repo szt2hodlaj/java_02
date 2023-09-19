39c67bcac107cae627685dcfcb7564888043e087
Hódi Lajos Gábor <115459590+szt2hodlaj@users.noreply.github.com>
2023-09-19 12:02:53 +0200

    Add files via upload

---
+++ Introduce.java
@@ -0,0 +1,11 @@
+public class Introduce {
+    public static void main(String[] args){
+        String nev = "John Doe";
+        int eletkor = 31;
+        double magassag = 1.87;
+
+        System.out.println(nev);
+        System.out.println(eletkor);
+        System.out.println(magassag);
+    }
+}
