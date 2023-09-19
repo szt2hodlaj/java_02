adbdda3b711442b90e5e20930a781d30ce363261
Hódi Lajos Gábor <115459590+szt2hodlaj@users.noreply.github.com>
2023-09-19 11:52:44 +0200

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,13 @@
+public class Basic {
+
+    public static void main(String[] args){
+        String nev = "Hódi Lajos Gábor";
+        int eletkor = 20;
+        double magassag = 1.75;
+        boolean hazas = false;
+        System.out.println("Név: " + nev);
+        System.out.println("Életkor: " + eletkor);
+        System.out.println("Magasság: " + magassag + " méter");
+        System.out.println("Házassági státusz: " + (hazas ? "Házas" : "Nem házas"));
+    }
+}
